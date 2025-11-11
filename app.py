b6c3b8488efbbaaadd956946b7f9e4223b1390e6
babyblueviper1 <babyblueviperbusiness@gmail.com>
2025-11-11 19:03:23 -0300

    Update app.py . nodes eternal

--- app.py
+++ app.py
@@ -209,8 +209,8 @@ if st.button("Ignite Swarm"):
     st.subheader("🜂 Exports")
     csv_data = get_csv_data(gci, i_ab, fidelity, alert, n_nodes, prune_pct, noise_sigma, motif_seed, motif_entropy)
     st.download_button("Download CSV", csv_data, "andes_grid_v7_motifs.csv", "text/csv")
-    blueprint_data = get_blueprint_data(gci, i_ab, fidelity, alert, n_nodes, prune_pct, noise_sigma, motif_seed, motif_entropy)
-    st.download_button("Download Blueprint JSON", blueprint_data, "seed_blueprints_v7_entry.json", "application/json")
+   blueprint_data = get_blueprint_data(gci, i_ab, fidelity, alert, n_nodes, prune_pct, noise_sigma, motif_seed, motif_entropy)
+st.download_button("Download Blueprint JSON", blueprint_data, "seed_blueprints_v7_entry.json", "application/json")
 
 st.markdown("---")
 st.markdown("Fork the Swarm: [GitHub](https://github.com/babyblueviper1/Viper-Stack-Omega) | Contact: babyblueviperbusiness@gmail.com")
