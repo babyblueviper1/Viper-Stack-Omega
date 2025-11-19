db708d73aa7df6ef40901ab4b0390962f94d2e9f
babyblueviper1 <babyblueviperbusiness@gmail.com>
2025-11-19 18:20:24 -0300

    Create sw.js . nodes eternal

---
+++ static/sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('omega-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
