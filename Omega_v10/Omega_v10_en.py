15baed37698a35e18f2349a6ce9c5740530f4f8e
babyblueviper1 <babyblueviperbusiness@gmail.com>
2025-11-30 14:50:11 -0300

    Update Omega_v10_en.py . nodes eternal

--- Omega_v10/Omega_v10_en.py
+++ Omega_v10/Omega_v10_en.py
@@ -794,7 +794,7 @@ with gr.Blocks(
                     border:2px solid #f7931a; border-radius:12px; font-size:0.95rem; color:#f7931a;
                     font-weight:600; text-align:center; box-shadow:0 4px 20px rgba(247,147,26,0.25);">
             No private keys ever entered • 100% non-custodial<br>
-            <span style="font-weight:800; color:#00ff9d;">Nothing is sent to any server</span> • Runs entirely in your browser
+            <span style="font-weight:800; color:#00ff9d;text-shadow: 0 0 12px black, 0 0 24px black;">Nothing is sent to any server</span> • Runs entirely in your browser
             </div>
             """)
             
